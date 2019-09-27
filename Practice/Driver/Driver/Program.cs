5952abe127271ff41a940b9b5d05d7075e3363a9
Aarif Jindani <JIndaniaarif@gmail.com>
2019-09-27 14:27:46 +0530

    Segment Tree

--- Practice/Driver/Driver/Program.cs
+++ Practice/Driver/Driver/Program.cs
@@ -22,7 +22,7 @@ namespace Driver
         static void Main(string[] args)
         {
             //LeetCode.ReversePair.Test();
-            Utils.RadixSort.Test();
+            Utils.SegmentTree.Test();
             /*int[] a = { 5, 4,6, 2, 1 };
             IComparer<int> comparer = new IntComparer();
             Utils.PriorityQueue<int> q = new Utils.PriorityQueue<int>(a,comparer);
@@ -41,8 +41,65 @@ namespace Driver
             //Console.WriteLine(Utils.SuffixArray.Get(new int[] { 3,2,3,4,1}));
             Console.ReadKey();
         }
+
+        public int MaxRepOpt1(string s)
+        {
+
+            int n = s.Length;
+            int i = 0;
+            int res = 1;
+            int[] first = new int[26];
+            int[] second = new int[26];
+            for (i = 0; i < n; i++)
+            {
+                first[s[n - i - 1] - 'a'] = n - i;
+                second[s[i] - 'a'] = i + 1;
+            }
+            while (i < n)
+            {
+                int nextWindow = GetNextWindow(s, i);
+                Console.WriteLine(nextWindow + ":" + i);
+                int index = s[i] - 'a';
+                if (first[index] != 0 && first[index] < i)
+                {
+                    res = Math.Max(res, nextWindow - i + 1);
+                    i = nextWindow + 1;
+                }
+                else if (second[index] != 0 && second[index] > i)
+                {
+                    res = Math.Max(res, nextWindow - i + 1);
+                    i = nextWindow + 1;
+                }
+                else
+                {
+                    res = Math.Max(res, nextWindow - i);
+                    i = nextWindow;
+                }
+
+
+            }
+            return res;
+
+        }
+        public int GetNextWindow(String s, int start)
+        {
+            bool missMatched = false;
+            int res = start;
+            for (int i = start + 1; i < s.Length; i++)
+            {
+                if (s[i - 1] != s[i])
+                {
+                    if (missMatched)
+                        return i - 1;
+                    else
+                        missMatched = true;
+                }
+            }
+            return s.Length - 1;
+        }
     }
 
-    
-    
+   
+
+
 }
---
+++ Practice/Driver/Utils/SegmentTree.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public class SegmentTree
+    {
+        int n;
+        int[] tree;
+
+        public static void Test()
+        {
+            int[] a =new int[] { 0,1, 2, 3, 4, 5, 6, 7 };
+            SegmentTree st = new SegmentTree(a);
+            Console.WriteLine(st.Query(0, 7));
+            Console.WriteLine(st.Query(0, 2));
+            Console.WriteLine(st.Query(2, 3));
+            Console.WriteLine(st.Query(2, 7));
+            st.Update(2, 10);
+
+            Console.WriteLine(st.Query(0, 7));
+            Console.WriteLine(st.Query(0, 2));
+            Console.WriteLine(st.Query(2, 3));
+            Console.WriteLine(st.Query(2, 7));
+            Console.WriteLine(st.Query(0, 0));
+        }
+        public SegmentTree(int[] a)
+        {
+            this.n = a.Length;
+            this.tree = new int[n*4];
+            BuildTree(0, a, 0, n-1);
+        }
+
+        private void BuildTree(int node, int[] a, int start, int end)
+        {
+            if (start == end)
+
+            {
+                tree[node] = a[start];
+                return;
+            }
+
+            int mid = start + (end - start) / 2;
+
+            BuildTree(node * 2+1, a, start, mid);
+            BuildTree(node * 2 + 2, a, mid + 1, end);
+
+            tree[node] = tree[node * 2+1] + tree[node * 2 + 2];
+        }
+
+        public int Query(int l, int r)
+        {
+            return Query(0, 0, n - 1, l, r);
+        }
+        private int Query(int node, int start, int end, int l, int r)
+        {
+            if (start > r || end < l)
+            {
+                return 0;
+            }
+
+            if (l <= start && end <= r)
+                return tree[node];
+
+            int mid = start + (end - start) / 2;
+            int p1 = Query(node * 2+1, start, mid, l, r);
+            int p2 = Query(node * 2 + 2, mid+1, end, l, r); ;
+            return p1 + p2;
+
+        }
+
+        public void Update(int id, int val)
+        {
+            Update(0, 0, n - 1, id, val);
+        }
+        private void Update(int node, int start, int end, int id, int val)
+        {
+            if(start == end)
+            {
+                tree[node] = val;
+                return;
+            }
+
+            int mid = start + (end - start) / 2;
+            if (id <= mid)
+                Update(node * 2+1, start, mid, id, val);
+            else
+                Update(node * 2 + 2, mid + 1, end, id, val);
+            tree[node] = tree[node * 2+1] + tree[node * 2 + 2];
+        }
+    }
+}
